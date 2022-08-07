d55b505c8696c0bcb932daf8bd3228277a2272c9
Ishaque Ahmed <ishaqueahmednasim@gmail.com>
2022-08-07 11:41:27 +0600

    view details

--- index.js
+++ index.js
@@ -2,21 +2,46 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./middlewares/replaceTemlates');
+
+const tempOver = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    if (pathName === '/' || pathName === '/overview') {
-        res.end('Hello from the OVERVIEW');
-    } else if (pathName === '/product') {
-        res.end('Hello from the PRODUCT');
-    } else if (pathName === '/api') {
+    const { query, pathname } = (url.parse(req.url, true));
+    // console.log(req.url);
+    // console.log(url.parse(req.url, true));
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+
+        const cardsHtml = dataObject.map(curObj => replaceTemplate(tempCard, curObj)).join('');
+        const output = tempOver.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        // console.log(output);
+        res.end(output);
+
+    } else if (pathname === '/product') {
+        // console.log(query);
+        res.writeHead(200, {
+            'Content-type': 'text/html',
+        });
+        const product = dataObject[query.id];
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output);
+
+    } else if (pathname === '/api') {
         res.writeHead(200, {
             'Content-type': 'application/json',
         });
         res.end(data);
     }
+
     else {
         res.writeHead(404, {
             'Content-type': 'text/html',
